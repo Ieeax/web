5219228e6162e8630a5d36589ea63fe125be18b1
Ieeax <dev@leeax.de>
2020-12-13 00:45:37 +0100

    Removed UnitTests for LxButton (for now)

--- tests/Leeax.Web.Components.Tests/ButtonTests.cs
+++ tests/Leeax.Web.Components.Tests/ButtonTests.cs
@@ -9,51 +9,51 @@ namespace Leeax.Web.Components.Tests
 {
     public class ButtonTests : TestContext
     {
-        [Fact]
-        public void HasText()
-        {
-            Services.AddComponents();
-            Services.AddModals();
-            Services.AddTheming(null);
+        //[Fact]
+        //public void HasText()
+        //{
+        //    Services.AddComponents();
+        //    Services.AddModals();
+        //    Services.AddTheming(null);
 
-            RenderTree.Add<LxAppScope>();
+        //    RenderTree.Add<LxAppScope>();
 
-            // Add button with text
-            var component = RenderComponent<LxButton>(
-                builder => builder.Add(x => x.Text, "TEXT"));
+        //    // Add button with text
+        //    var component = RenderComponent<LxButton>(
+        //        builder => builder.Add(x => x.Text, "TEXT"));
 
-            // Ensure that no icon is displayed
-            Assert.Null(component.Find(".lx-icon"));
+        //    // Ensure that no icon is displayed
+        //    Assert.Null(component.Find(".lx-icon"));
 
-            // Ensure right text
-            var spanNode = component.Find("span");
-            Assert.Equal("TEXT", spanNode.TextContent);
-        }
+        //    // Ensure right text
+        //    var spanNode = component.Find("span");
+        //    Assert.Equal("TEXT", spanNode.TextContent);
+        //}
 
-        [Fact]
-        public void HasIcon()
-        {
-            Services.AddComponents();
-            Services.AddModals();
-            Services.AddTheming(null);
+        //[Fact]
+        //public void HasIcon()
+        //{
+        //    Services.AddComponents();
+        //    Services.AddModals();
+        //    Services.AddTheming(null);
 
-            RenderTree.Add<LxAppScope>();
+        //    RenderTree.Add<LxAppScope>();
 
-            // Add button with icon
-            var component = RenderComponent<LxButton>(
-                builder => builder.Add(
-                    x => x.ChildContent, 
-                    x =>
-                    {
-                        x.OpenComponent<LxButtonIcon>(0);
-                        x.CloseComponent();
-                    }));
+        //    // Add button with icon
+        //    var component = RenderComponent<LxButton>(
+        //        builder => builder.Add(
+        //            x => x.ChildContent, 
+        //            x =>
+        //            {
+        //                x.OpenComponent<LxButtonIcon>(0);
+        //                x.CloseComponent();
+        //            }));
 
-            // Ensure that a icon is displayed
-            Assert.NotNull(component.Find(".lx-icon"));
+        //    // Ensure that a icon is displayed
+        //    Assert.NotNull(component.Find(".lx-icon"));
 
-            // Ensure no text
-            Assert.Null(component.Find("span"));
-        }
+        //    // Ensure no text
+        //    Assert.Null(component.Find("span"));
+        //}
     }
 }
