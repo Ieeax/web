a0caef80ed5af3cae1fb103d4268fcbb6e65c7d4
Ieeax <dev@leeax.de>
2020-12-15 19:47:31 +0100

    Added bootstrapper for loading ES6 modules at startup

---
+++ src/Leeax.Web.Components.Abstractions/ModuleBootstrapper.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Leeax.Web.Components.Abstractions
+{
+    /// <summary>
+    /// Imports a javascript ES6 module and stores it in the registered <see cref="IJSObjectReferenceStore"/>.
+    /// </summary>
+    public class ModuleBootstrapper : IBootstrapper
+    {
+        private readonly IEnumerable<ModuleInfo> _modules;
+
+        public ModuleBootstrapper(ModuleInfo module)
+        {
+            _modules = new ModuleInfo[] { module };
+        }
+
+        public ModuleBootstrapper(IEnumerable<ModuleInfo> modules)
+        {
+            _modules = modules ?? throw new ArgumentNullException(nameof(modules));
+        }
+
+        public async Task<int> RunAsync(IServiceProvider serviceProvider)
+        {
+            var jsRuntime = serviceProvider.GetRequiredService<IJSRuntime>();
+            var store = serviceProvider.GetRequiredService<IJSObjectReferenceStore>();
+
+            foreach (var curModule in _modules)
+            {
+                if (jsRuntime is IJSInProcessRuntime)
+                {
+                    await jsRuntime.ImportModuleAsync<IJSInProcessObjectReference>(
+                        curModule.Path,
+                        curModule.Name,
+                        store);
+                }
+                else
+                {
+                    await jsRuntime.ImportModuleAsync(
+                        curModule.Path,
+                        curModule.Name,
+                        store);
+                }
+            }
+
+            return 0;
+        }
+
+        public string Name => "ES6 module bootstrapper";
+    }
+}
---
+++ src/Leeax.Web.Components.Abstractions/ModuleInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Leeax.Web.Components.Abstractions
+{
+    public readonly struct ModuleInfo
+    {
+        public ModuleInfo(string path)
+            : this(path, null)
+        {
+        }
+
+        public ModuleInfo(string path, string? name)
+        {
+            Path = path ?? throw new ArgumentNullException(nameof(path));
+            Name = name ?? path;
+        }
+
+        public string Path { get; }
+
+        public string Name { get; }
+    }
+}
