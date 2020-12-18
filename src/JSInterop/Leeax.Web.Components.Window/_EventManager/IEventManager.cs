c63507103e6abf1e04223ce432668a08fa0d1595
Ieeax <dev@leeax.de>
2020-12-18 18:17:42 +0100

    Added some XML comments

--- src/JSInterop/Leeax.Web.Components.Window/_EventManager/IEventManager.cs
+++ src/JSInterop/Leeax.Web.Components.Window/_EventManager/IEventManager.cs
@@ -5,12 +5,36 @@ namespace Leeax.Web.Components.Window
 {
     public interface IEventManager
     {
+        /// <summary>
+        /// Adds the passed <paramref name="handler"/> for the event with the given <paramref name="eventName"/>.
+        /// </summary>
+        /// <typeparam name="TArgument">The type of the argument.</typeparam>
+        /// <param name="eventName">The name of the event for which the handler should be added.</param>
+        /// <param name="handler">The handler to invoke whenever the event is raised.</param>
         void AddEventHandler<TArgument>(string eventName, Action<TArgument?> handler) where TArgument : EventArgs;
 
+        /// <summary>
+        /// Adds the passed <paramref name="handler"/> for the event with the given <paramref name="eventName"/>.
+        /// </summary>
+        /// <typeparam name="TArgument">The type of the argument.</typeparam>
+        /// <param name="eventName">The name of the event for which the handler should be added.</param>
+        /// <param name="handler">The handler to invoke whenever the event is raised.</param>
         Task AddEventHandlerAsync<TArgument>(string eventName, Action<TArgument?> handler) where TArgument : EventArgs;
 
+        /// <summary>
+        /// Removes the passed <paramref name="handler"/> from the event with the given <paramref name="eventName"/>.
+        /// </summary>
+        /// <typeparam name="TArgument">The type of the argument.</typeparam>
+        /// <param name="eventName">The name of the event for which the handler should be removed.</param>
+        /// <param name="handler">The handler to remove.</param>
         void RemoveEventHandler<TArgument>(string eventName, Action<TArgument?> handler) where TArgument : EventArgs;
 
+        /// <summary>
+        /// Removes the passed <paramref name="handler"/> from the event with the given <paramref name="eventName"/>.
+        /// </summary>
+        /// <typeparam name="TArgument">The type of the argument.</typeparam>
+        /// <param name="eventName">The name of the event for which the handler should be removed.</param>
+        /// <param name="handler">The handler to remove.</param>
         Task RemoveEventHandlerAsync<TArgument>(string eventName, Action<TArgument?> handler) where TArgument : EventArgs;
     }
 }
