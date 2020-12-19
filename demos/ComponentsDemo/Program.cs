8003c83e5f8d01b99255aeedf47003dc71749310
Ieeax <dev@leeax.de>
2020-12-19 17:05:05 +0100

    Added (optional) default animation for initial loading

--- demos/ComponentsDemo/Program.cs
+++ demos/ComponentsDemo/Program.cs
@@ -18,7 +18,7 @@ namespace ComponentsDemo
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             // Add all basic services of the library
