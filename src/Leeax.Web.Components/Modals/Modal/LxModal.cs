41a4a53fb2a02952420ded5a737532dabd77b4a2
leeax <65013908+Ieeax@users.noreply.github.com>
2022-08-22 15:36:32 +0000

    Updated transitions / animations of LxModal

--- src/Leeax.Web.Components/Modals/Modal/LxModal.cs
+++ src/Leeax.Web.Components/Modals/Modal/LxModal.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Leeax.Web.Builders;
-using Leeax.Web.Components.Abstractions;
 using Leeax.Web.Components.DOM;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Rendering;
@@ -11,21 +9,21 @@ namespace Leeax.Web.Components.Modals
     public class LxModal : LxComponentBase
     {
         public const string ClassName = "lx-modal";
-        
+
         private ElementReference _modalReference;
         private long _callbackId = -1;
-        
+
         protected override void BuildAttributeSet(AttributeSetBuilder builder)
         {
             builder.AddClassAttribute(x => x
-                .AddMultiple(ClassName, "overflow-hidden", "flex-col", "lx-elevation-l5")
+                .AddMultiple(ClassName, "overflow-hidden", "flex-col", "lx-elevation-l6")
                 .AddAlignment(Alignment));
         }
 
         protected override void BuildRenderTree(RenderTreeBuilder builder)
         {
             base.BuildRenderTree(builder);
-            
+
             builder.OpenElement(0, "div");
             builder.AddMultipleAttributes(1, AttributeSet);
             builder.AddElementReferenceCapture(2, element => _modalReference = element);
@@ -48,7 +46,7 @@ namespace Leeax.Web.Components.Modals
             {
                 // Register callback if user clicks outside of dropdown
                 _callbackId = await ElementService.AddClickOutsideOfElementHandlerAsync(
-                    new[] { _modalReference }, 
+                    new[] { _modalReference },
                     () =>
                     {
                         _callbackId = -1;
@@ -56,12 +54,12 @@ namespace Leeax.Web.Components.Modals
                     });
             }
         }
-        
+
         [Inject]
         private IElementService ElementService { get; set; } = null!;
 
         #region Parameters
-        
+
         /// <summary>
         /// Gets or sets the alignment of the modal.
         /// The default value is <see cref="Alignment.Center"/>.
@@ -75,10 +73,10 @@ namespace Leeax.Web.Components.Modals
         /// </summary>
         [Parameter]
         public bool EnableClickOutside { get; set; } = true;
-        
+
         [Parameter]
         public RenderFragment? ChildContent { get; set; }
-        
+
         [CascadingParameter]
         public ModalState? Context { get; set; }
         #endregion
